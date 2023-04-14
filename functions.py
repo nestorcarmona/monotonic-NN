5dff1c8ba3a0253c03ad929578f17a1dced600e2
Nestor Carmona <nestor.carmona97@gmail.com>
2023-04-14 20:59:33 +0200

    add descriptions

--- functions.py
+++ functions.py
@@ -1,12 +1,17 @@
-# defining the function
+# defining the functions
+
+# monotonic increasing in x
 def f1(x):
     return (x+1)**2
 
+# monotonic decreasing in x
 def f2(x):
     return -(x+1)**2
 
+# monotonic increasing in x and y
 def f3(x, y):
     return x+y
 
+# monotonic increasing in x and decreasing in y
 def f4(x, y):
     return x-y
