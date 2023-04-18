5732358998ad9c49c880e35453cd1ca4409a5f31
Nestor Carmona <nestor.carmona97@gmail.com>
2023-04-18 10:57:33 +0200

    main update

--- models/monNN.py
+++ models/monNN.py
@@ -2,17 +2,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
-
-class Monotonic(nn.Module):
-    def __init__(self, latent):
-        super().__init__()
-        self.latent = latent
-
-    def getweights(self):
-        return torch.exp(self.latent)
+from typing import List, Union
 
 class MonotonicGroup(nn.Module):
-    def __init__(self, in_features, out_features, increasing):
+    def __init__(
+            self, 
+            in_features: int, 
+            out_features: int, 
+            increasing: List[bool]) -> None:
+        
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -22,96 +20,97 @@ class MonotonicGroup(nn.Module):
         self.latent_weights = nn.Parameter(torch.Tensor(out_features, in_features).uniform_(-stdv, stdv))
         self.latent_bias = nn.Parameter(torch.Tensor(out_features).uniform_(-stdv, stdv))
 
-        self.weights = Monotonic(self.latent_weights)
+        self.weight = nn.Parameter(torch.Tensor(out_features, in_features)) # Monotonic(self.latent_weights)
 
-        self.bias = Monotonic(self.latent_bias)
+        self.bias = nn.Parameter(torch.Tensor(out_features)) # Monotonic(self.latent_bias)
 
-    def forward(self, x):
-        if self.increasing:
-            return F.linear(x, self.weights.getweights(), self.bias.getweights())
-        else:
-            return F.linear(x, -self.weights.getweights(), -self.bias.getweights())
+        self.reset_parameters()
 
+    def reset_parameters(self) -> None:
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+        bound = 1 / math.sqrt(fan_in)
+        nn.init.uniform_(self.bias, -bound, bound)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        weight = self.weight.clone()
+
+        for i, incr in enumerate(self.increasing):
+            if incr:
+                weight[:, i] = torch.exp(weight[:, i])
+            else:
+                weight[:, i] = - torch.exp(weight[:, i])
+
+        return nn.functional.linear(x, weight, self.bias)
 
 class MonotonicLinear(nn.Module):
-    def __init__(self, groups):
+    def __init__(self, groups: List[MonotonicGroup]) -> None:
         super().__init__()
         self.groups = groups
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         output = [g.forward(x) for g in self.groups]
         return torch.cat(output, dim=0).reshape(len(self.groups), x.shape[0], self.groups[0].out_features)
 
-class MonotonicMax(nn.Module):
-    def __init__(self):
+class MaxLayer(nn.Module):
+    def __init__(self, num_groups: int, group_sizes: List[int]) -> None:
         super().__init__()
-
-    def forward(self, x):
-        return torch.cat(tuple(torch.max(i, dim=1)[0].unsqueeze(1) for i in x), dim=1)
-
-class MonotonicMin(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x):
-        return torch.min(x, dim=1)[0].unsqueeze(1)
-    
-class MonotonicInteraction(nn.Module):
-    def __init__(self, input_size):
-        super().__init__()
-        self.input_size = input_size
-        self.interaction_layer = nn.Linear(input_size, input_size, bias=False)
-
-    def forward(self, x):
-        x = self.interaction_layer(x)
-        return x
-    
-class MonotonicNet(nn.Module):
-    def __init__(self, input_size, num_groups, group_sizes, increasing, monotonic):
-        super().__init__()
-        self.input_size = input_size
         self.num_groups = num_groups
         self.group_sizes = group_sizes
-        self.increasing = increasing
-        self.monotonic = monotonic
-
-
-        self.layers = nn.ModuleList()
-        for i in range(input_size):
-            groups = []
-            if monotonic[i]:
-                for j in range(num_groups[i]):
-                    group = MonotonicGroup(1, group_sizes[i], increasing[i])
-                    groups.append(group)
-            else:
-                for j in range(num_groups[i]):
-                    group = nn.Linear(1, group_sizes[i])
-                    groups.append(group)
-
-            self.layers.append(MonotonicLinear(nn.ModuleList(groups)))
-                
-        # in multimensional, take into account the interaction between variables
-        if input_size > 1:
-            self.interaction_layer = nn.Linear(sum(group_sizes), sum(group_sizes))
-
-        self.m2 = MonotonicMax()
-        self.m3 = MonotonicMin()
-
-    def forward(self, x):
-        x1 = []
-        for i, layer in enumerate(self.layers):
-            if self.monotonic[i]:
-                x1.append(layer(x[:, i].unsqueeze(1)))
-            else:
-                x1.append(layer(x[:, i].unsqueeze(1)))
-
-        x = torch.cat(x1, dim=2)
-
-        # in multimensional, take into account the interaction between variables
-        if self.input_size > 1:
-            x = self.interaction_layer(x)
-
-        x = self.m2(x)
-        x = self.m3(x)
         
-        return x
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = []
+        # loop through each group
+        for i in range(self.num_groups):
+            group = x[:, :self.group_sizes[i]]
+            out.append(torch.max(group, dim=1, keepdim=True)[0])
+            x = x[:, self.group_sizes[i]:]
+        out = torch.cat(out, dim=1)
+        return out
+
+class MinLayer(nn.Module):
+    def __init__(self, in_features: int, out_features: int) -> None:
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(in_features, out_features))
+        self.bias = nn.Parameter(torch.zeros(out_features))
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # not sure if necessary
+        out = x @ self.weight + self.bias
+        out = torch.min(out, dim=1, keepdim=True)[0]
+        return out
+
+
+class MonotonicNet(nn.Module):
+    def __init__(
+            self, 
+            in_features: int, 
+            group_sizes: List[int], 
+            increasing: List[bool]) -> None:
+        
+        super().__init__()
+        self.in_features = in_features
+        self.num_groups = len(group_sizes)
+        self.group_sizes = group_sizes
+        self.increasing = increasing
+        
+        # Hidden layer: linear unit
+        self.layer1 = MonotonicGroup(in_features=in_features, out_features=sum(group_sizes), increasing=increasing)
+
+        # batch normalization
+        # self.bn1 = nn.BatchNorm1d(sum(group_sizes))
+
+        # Hidden layer: max layer
+        self.max_layer = MaxLayer(num_groups=self.num_groups, group_sizes=self.group_sizes)
+        
+        # Output layer: min layer
+        self.min_layer = MinLayer(in_features=self.num_groups, out_features=1)
+       
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        out = self.layer1(x)
+        # out = self.bn1(out)
+        out = self.max_layer(out)
+        out = self.min_layer(out)
+        return out
--- models/unn_loss.py
+++ models/unn_loss.py
@@ -7,7 +7,7 @@ import math
 # define a dense neural network with 2 hidden layers
 class FCNet(nn.Module):
     def __init__(self, in_features: int, out_features: int, n_hidden: list = [8, 8]) -> None:
-        super(FCNet, self).__init__()
+        super().__init__()
 
         self.net = []
 
@@ -17,12 +17,12 @@ class FCNet(nn.Module):
             self.net.extend(
                 [
                     nn.Linear(h0, h1),
-                    nn.BatchNorm1d(h1),
+                    # nn.BatchNorm1d(h1),
                     nn.Sigmoid()
                 ]
             )
         
-        self.net.pop() # pop last batchnorm
+        # self.net.pop() # pop last batchnorm
         self.net.pop() # pop last activation function for output layer
         self.net = nn.Sequential(*self.net)
 
@@ -32,15 +32,12 @@ class FCNet(nn.Module):
     
     # define the monotonicity loss: compute divergence of output w.r.t. input
     def monotonicity_loss(self, x: torch.Tensor, M: torch.Tensor) -> torch.Tensor:
-        # print('loss')
         x.requires_grad_(True)
         y = self.forward(x)
 
         gradient = torch.autograd.grad(y, x, grad_outputs=torch.ones_like(y), create_graph=True, allow_unused=True)[0]
-        # print("gradient shape: ", gradient.shape)
         
         selected_gradient = torch.mm(gradient, M)
-        # print("selected gradient shape: ", selected_gradient.shape)
         directional_derivative = torch.sum(selected_gradient, dim=1)
         loss = torch.sum(torch.max(torch.zeros_like(directional_derivative), -directional_derivative))
 
